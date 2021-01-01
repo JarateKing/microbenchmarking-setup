6d8b0e941219f0b34ce4f0ca348090f0d11faec1
JarateKing <scmacintosh@upei.ca>
2021-01-01 17:19:48 -0400

    made functional benchmark

--- benchmarks/benchmark.h
+++ benchmarks/benchmark.h
@@ -1,15 +1,39 @@
 #include <iostream>
+#include <chrono>
+#include <iomanip>
 
 // obtain a unique identifier
 #define NEWNAME_CONCAT2(x,y) x##y
 #define NEWNAME_CONCAT1(x,y) NEWNAME_CONCAT2(x,y)
 #define NEWNAME NEWNAME_CONCAT1(bench, __COUNTER__)
 
-#define BENCHMARK(id, data)                                    \
-const bool NEWNAME = [](){                                     \
-	std::string ID = #id ;                                     \
-	struct Internals data internals;                           \
-	std::cout << ID << ": " << internals.description << '\n';  \
-	return 0;                                                  \
+// benchmark method
+#define BENCHMARK(id, data)                                                                   \
+const bool NEWNAME = [](){                                                                    \
+	std::string ID = #id ;                                                                    \
+	struct Internals data internals;                                                          \
+																							  \
+	for (int i = 0; i < 5; i++) {                                                             \
+		internals.Pre();                                                                      \
+		internals.Run();                                                                      \
+		internals.Post();                                                                     \
+	}                                                                                         \
+																							  \
+	std::chrono::high_resolution_clock::time_point t1, t2;                                    \
+	long double avg = 0;                                                                      \
+	for (int i = 0; i < 200; i++) {                                                           \
+		internals.Pre();                                                                      \
+																							  \
+		t1 = std::chrono::high_resolution_clock::now();                                       \
+		internals.Run();                                                                      \
+		t2 = std::chrono::high_resolution_clock::now();                                       \
+		avg += std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();         \
+																							  \
+		internals.Post();                                                                     \
+	}                                                                                         \
+																							  \
+	std::cout << std::setprecision(40);										  				  \
+	std::cout << ID << ": " << avg / 200 << '\n';                                             \
+	return 0;                                                                                 \
 }();
 
--- benchmarks/example1.cpp
+++ benchmarks/example1.cpp
@@ -1,5 +1,25 @@
 #include "benchmark.h"
+#include <vector>
+#include <algorithm>
 
 BENCHMARK( Example One, {
+	// metadata
 	std::string description = "Description for Example One";
+	
+	// custom data
+	std::vector<int> arr;
+	
+	// methods
+	void Pre() {
+		arr.resize(1000000);
+		for (int i = 0; i < 1000000; i++) {
+			arr[i] = rand();
+		}
+	}
+	void Run() {
+		std::sort(arr.begin(), arr.end());
+	}
+	void Post() {
+		
+	}
 });
