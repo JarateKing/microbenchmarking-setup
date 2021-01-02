3e89283b2742e56e175a988c040ff22711b67950
JarateKing <scmacintosh@upei.ca>
2021-01-02 13:27:58 -0400

    fixed 0s failing with logarithm

--- benchmarks/plot.py
+++ benchmarks/plot.py
@@ -10,7 +10,8 @@ for i in range(0, len(inputs), 3):
 	labels.append(inputs[i])
 	x = list(map(int, inputs[i+1].split()))
 	y = list(map(int, inputs[i+2].split()))
-	plt.plot(x, y, 'r-s')
+	y = [v+0.01 for v in y]
+	plt.plot(x, y, '-s', label=inputs[i])
 
 plt.yscale('log')
 plt.xscale('log')
