e2f5f979cc3b20c98c321d5b7208ab8f51823114
JarateKing <scmacintosh@upei.ca>
2021-01-02 01:23:44 -0400

    plot new format

--- benchmarks/plot.py
+++ benchmarks/plot.py
@@ -1,14 +1,17 @@
 import sys
+import matplotlib.pyplot as plt
 
 inputs = []
 for line in sys.stdin:
-    inputs.append(line.strip())
+	inputs.append(line.strip())
 
 labels = []
-datapoints = []
-for i in range(0, len(inputs), 2):
-    labels.append(inputs[i])
-    datapoints.append(list(map(int, inputs[i+1].split())))
+for i in range(0, len(inputs), 3):
+	labels.append(inputs[i])
+	x = list(map(int, inputs[i+1].split()))
+	y = list(map(int, inputs[i+2].split()))
+	plt.plot(x, y, 'r-s')
 
-print(labels)
-print(datapoints)
+plt.yscale('log')
+plt.xscale('log')
+plt.show()
