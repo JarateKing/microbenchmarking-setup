6fedb66c852b7ee12de5621996a8fa77f934134c
JarateKing <scmacintosh@upei.ca>
2022-09-13 09:03:01 -0300

    added random base generator

--- benchmarks/benchmarking_generators.h
+++ benchmarks/benchmarking_generators.h
@@ -1,15 +1,39 @@
 #include <vector>
+#include <chrono>
+#include <random>
 
 namespace benchmarking {
+	std::mt19937 rng;
+	
 	struct Dataset {
 		std::vector<int> arr;
 	};
 	
-	Dataset index(int n, int increment = 1, int base = 0) {
+	void setup() {
+		rng.seed((uint32_t)std::chrono::steady_clock::now().time_since_epoch().count());
+	}
+	
+	Dataset _getBaseDataset(int n) {
 		Dataset toret;
 		toret.arr.resize(n);
+		return toret;
+	}
+	
+	Dataset index(int n, int increment = 1, int base = 0) {
+		Dataset toret = _getBaseDataset(n);
+		
 		for (int i = 0; i < n; i++)
 			toret.arr[i] = base + (i * increment);
+		
+		return toret;
+	}
+	
+	Dataset random(int n) {
+		Dataset toret = _getBaseDataset(n);
+		
+		for (int i = 0; i < n; i++)
+			toret.arr[i] = rng();
+		
 		return toret;
 	}
 }
--- benchmarks/example.cpp
+++ benchmarks/example.cpp
@@ -2,7 +2,9 @@
 #include <iostream>
 
 int main() {
-	benchmarking::Dataset dataset = benchmarking::index(10);
+	benchmarking::setup();
+	
+	benchmarking::Dataset dataset = benchmarking::random(10);
 	
 	for (int i = 0; i < 10; i++)
 		std::cout << dataset.arr[i] << ' ';
